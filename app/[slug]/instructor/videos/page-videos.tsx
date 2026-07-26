a2ccba985426b228f39e19c7b7017d9e9c8ed0cf
Rick Stitzer <teetogreenapex2@gmail.com>
2026-07-26 12:44:00 -0400

    Replace page.tsx with clean version

--- app/[slug]/instructor/videos/page-videos.tsx
+++ app/[slug]/instructor/videos/page-videos.tsx
@@ -1,12 +1,9 @@
 import { getServerSession } from "next-auth";
+import { loginRedirectUrl } from "@/lib/businessUrl";
 import { redirect, notFound } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import { getBusinessBySlug, requireMembership, getBasePaths } from "@/lib/tenant";
 import InstructorVideosClient from "./InstructorVideosClient";
-
-import { loginRedirectUrl } from "@/lib/businessUrl";
-
-
 
 export default async function InstructorVideosPage({ params }: { params: { slug: string } }) {
   const session = await getServerSession(authOptions);
@@ -18,5 +15,6 @@ export default async function InstructorVideosPage({ params }: { params: { slug:
   const membership = await requireMembership((session.user as any).id, business.id, ["owner", "instructor"]);
   const { basePath, apiBase } = getBasePaths(params.slug);
   if (!membership) redirect(`${basePath}/book`);
-return <InstructorVideosClient slug={params.slug} basePath={basePath} apiBase={apiBase} viewerMembershipId={membership.id} />;
+
+  return <InstructorVideosClient slug={params.slug} basePath={basePath} apiBase={apiBase} viewerMembershipId={membership.id} />;
 }
