8fae06fdc002354010efe4a866ebfdfa0f8168d1
Rick Stitzer <teetogreenapex2@gmail.com>
2026-07-26 12:36:55 -0400

    Fix missing page.tsx update for video upload feature

--- app/[slug]/instructor/videos/page.tsx
+++ app/[slug]/instructor/videos/page.tsx
@@ -6,6 +6,8 @@ import InstructorVideosClient from "./InstructorVideosClient";
 
 import { loginRedirectUrl } from "@/lib/businessUrl";
 
+import { loginRedirectUrl } from "@/lib/businessUrl";
+
 export default async function InstructorVideosPage({ params }: { params: { slug: string } }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect(loginRedirectUrl(`/${params.slug}/instructor/videos`));
@@ -16,6 +18,5 @@ export default async function InstructorVideosPage({ params }: { params: { slug:
   const membership = await requireMembership((session.user as any).id, business.id, ["owner", "instructor"]);
   const { basePath, apiBase } = getBasePaths(params.slug);
   if (!membership) redirect(`${basePath}/book`);
-
- return <InstructorVideosClient slug={params.slug} basePath={basePath} apiBase={apiBase} viewerMembershipId={membership.id} />;
+return <InstructorVideosClient slug={params.slug} basePath={basePath} apiBase={apiBase} viewerMembershipId={membership.id} />;
 }
